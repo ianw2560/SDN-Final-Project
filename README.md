4587e323f913d96134b4c030fb6e8cb5474188e1
ianw2560 <ianw2560@gmail.com>
2023-03-12 22:49:11 -0400

    Mininet creation works now

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # SDN Final Project
 
+## Usage
+
 Create Mininet topology
 ```
-sudo mn --topo mytopo --custom syn_flood_network_topo.py --controller=remote,ip=127.0.0.1,port=6653 --switch ovs,protocols=OpenFlow13
+sudo mn --topo mytopo --custom syn_flood_network_topo.py --controller=remote,ip=127.0.0.1,port=6653 --switch ovs,protocols=OpenFlow13 --link tc
 ```
 
 Start floodlight
--- syn_flood_network_topo.py
+++ syn_flood_network_topo.py
@@ -1,6 +1,7 @@
 from mininet.net import Mininet
 from mininet.node import Controller, OVSSwitch
 from mininet.topo import Topo
+from mininet.link import TCLink
 
 class MyTopo(Topo):
     def __init__(self):
@@ -9,32 +10,33 @@ class MyTopo(Topo):
 
         # Add switches
         switches = {}
-        for i in range(0, 11):
-            switch = self.addSwitch('s{}'.format(i+1), cls=OVSSwitch)
-            switches[i] = switch
+        for i in range(1, 11 + 1):
+            switch = self.addSwitch('s{}'.format(i), cls=OVSSwitch)
+            switches[i-1] = switch
 
         # Add TCP server connected to S8
         server = self.addHost('tcp_server')
         self.addLink(server, switches[7])
 
         # Add 30 hosts connected to S1-S4
-        for i in range(1, 5):
-            for j in range(1, 31):
+        for i in range(1, 4 +1):
+            for j in range(1, 30 + 1):
                 host = self.addHost('h{}_{}'.format(i, j))
-                self.addLink(host, switches[i], bw=100)
+                self.addLink(host, switches[i-1], bw=100)
 
-        # Add links between switches
-        for i in range(0, 4):
-            self.addLink(switches[i], switches[i+8], bw=1000)
+        # Add links between switches S1-S9 S2-S10 S3-S11 S4-S5
+        for i in range(1, 3+1):
+            self.addLink(switches[i-1], switches[i+7-1], bw=1000)
+        self.addLink(switches[4-1], switches[5-1], bw=1000)
             
         # Add links between switches
-        for i in range(5, 10):
-            self.addLink(switches[i], switches[i+1], bw=1000)
-        self.addLink(switches[10], switches[5], bw=1000)
+        for i in range(5, 10+1):
+            self.addLink(switches[i-1], switches[i], bw=1000)
+        self.addLink(switches[11-1], switches[5-1], bw=1000)
         
         # Add remaining switch connections
-        self.addLink(switches[7], switches[4], bw=1000)
-        self.addLink(switches[8], switches[5], bw=1000)
+        self.addLink(switches[8-1], switches[5-1], bw=1000)
+        self.addLink(switches[9-1], switches[6-1], bw=1000)
         
         
 
