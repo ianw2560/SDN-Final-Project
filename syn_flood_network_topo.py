e6c1e5ebdd77c564a05b29e06af29bc514344e70
ianw2560 <ianw2560@gmail.com>
2023-03-25 16:33:24 -0400

    Network topoology is correct now

--- syn_flood_network_topo.py
+++ syn_flood_network_topo.py
@@ -4,6 +4,7 @@ from mininet.topo import Topo
 from mininet.link import TCLink
 
 class MyTopo(Topo):
+
     def __init__(self):
         # Initialize topology
         Topo.__init__(self)
@@ -18,11 +19,16 @@ class MyTopo(Topo):
         server = self.addHost('tcp_server')
         self.addLink(server, switches[7])
 
-        # Add 30 hosts connected to S1-S4
-        for i in range(1, 4 +1):
+        # Add 30 hosts connected to S1-S3
+        for i in range(1, 3 +1):
             for j in range(1, 30 + 1):
                 host = self.addHost('h{}_{}'.format(i, j))
                 self.addLink(host, switches[i-1], bw=100)
+
+        # Add 32 hosts connected to S4
+        for i in range(1, 32 + 1):
+            host = self.addHost('h{}_{}'.format(4, i))
+            self.addLink(host, switches[4-1], bw=100)
 
         # Add links between switches S1-S9 S2-S10 S3-S11 S4-S5
         for i in range(1, 3+1):
@@ -37,8 +43,5 @@ class MyTopo(Topo):
         # Add remaining switch connections
         self.addLink(switches[8-1], switches[5-1], bw=1000)
         self.addLink(switches[9-1], switches[6-1], bw=1000)
-        
-        
-
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
