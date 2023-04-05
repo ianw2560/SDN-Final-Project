c07a2f9c2deb23df67f6dea3dc93b0c0908d3436
ianw2560@gmail.com <None>
2023-04-04 22:25:21 -0700

    Update current version of ModifiedSLICOTS.java

--- ModifiedSLICOTS.java
+++ ModifiedSLICOTS.java
@@ -3,11 +3,14 @@ package net.floodlightcontroller.modifiedslicots;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
  
+import org.projectfloodlight.openflow.protocol.OFFactories;
 import org.projectfloodlight.openflow.protocol.OFFactory;
 import org.projectfloodlight.openflow.protocol.OFFlowAdd;
+import org.projectfloodlight.openflow.protocol.OFFlowMod;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.DatapathId;
@@ -16,21 +19,34 @@ import org.projectfloodlight.openflow.types.IPv4Address;
 import org.projectfloodlight.openflow.types.IpProtocol;
 import org.projectfloodlight.openflow.types.MacAddress;
 import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.OFBufferId;
 import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.U64;
+import org.projectfloodlight.openflow.types.VlanVid;
 import org.projectfloodlight.openflow.protocol.OFPacketOut;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
 import org.projectfloodlight.openflow.protocol.instruction.OFInstructionApplyActions;
+import org.projectfloodlight.openflow.protocol.match.Match;
 import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.protocol.match.MatchFields;
  
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.internal.IOFSwitchService;
+import net.floodlightcontroller.core.internal.OFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 
 import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.devicemanager.IDevice;
+import net.floodlightcontroller.devicemanager.IDeviceService;
+
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.Set;
 import net.floodlightcontroller.packet.Ethernet;
@@ -42,12 +58,14 @@ import org.slf4j.LoggerFactory;
 
 public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	
-	protected IFloodlightProviderService floodlightProvider;
+	private static IFloodlightProviderService floodlightProvider;
+	private static ITopologyService floodlightTopology;
+	
 	protected static Logger logger;
 	private List<Connection> pendingList;
 	protected int K;
 	protected int hard_timeout;
-	private HashMap<MacAddress, Integer> C_map;
+	private ConcurrentHashMap<MacAddress, Integer> C_map;
 	
     @Override
     public String getName() {
@@ -77,9 +95,11 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	@Override
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
 	    floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+	    floodlightTopology = context.getServiceImpl(ITopologyService.class);
+	    
 	    logger = LoggerFactory.getLogger(ModifiedSLICOTS.class);
 	    pendingList = Collections.synchronizedList(new ArrayList<Connection>());
-	    C_map = new HashMap<MacAddress, Integer>();
+	    C_map = new ConcurrentHashMap<MacAddress, Integer>();
 	    K = 50;
 	    hard_timeout = 3;
 	}
@@ -93,10 +113,6 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
 	    switch (msg.getType()) {
 	    case PACKET_IN:
-	    	
-	    	
-	    	
-	    	//PacketHandler ph = new PacketHandler(sw, msg, cntx);
 	    	
 	        /* Retrieve the deserialized packet in message */
 	        Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
@@ -119,15 +135,15 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	                MacAddress srcMac = eth.getSourceMACAddress();
 	                MacAddress dstMac = eth.getDestinationMACAddress();
 	                
+	                // Get src dpid
+	                DatapathId srcDpid = sw.getId();
+
 	                short flags = tcp.getFlags();
+	                int C;
 	                
 	                if (C_map.containsKey(srcMac)) {
 	                	C_map.put(srcMac, 0);
 	                }
-	                
-	                
-	                DatapathId dpid = sw.getId();
-	                int C;
 	                
 	                // Is SYN?
 	                if (tcp.getFlags() == 2) {
@@ -141,6 +157,7 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	                	
 	                	if (C > K) {
 	                		// Install forwarding rule to block the requested host
+	                		
 	                	} else {
 		                	Connection record = new Connection(srcIp, dstIp, srcMac, dstMac, "SYN");
 		                	
@@ -148,6 +165,7 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 		                	pendingList.add(record);
 		                	
 		                	// Install temporary forwarding rule between client and server
+		                	this.installTemporaryForwardingRules(srcDpid, dstDpid, sw);
 	                	}
 	                	
 	                // Is RST?
@@ -175,31 +193,24 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	                	this.setPendingListStatus(srcIp, dstIp, srcMac, dstMac, "SYN-ACK");
 	                	
 	                	// Install temporary forwarding rule between client and server
+	                	this.installTemporaryForwardingRules(srcDpid, dstDpid, sw);
 	                
 	                // Is ACK?
 	                } else if (tcp.getFlags() == 16) {
 	                	
-	                	logger.info("[Switch= {}][SrcMAC = {}] ACK", dpid.toString(), srcMac.toString());
-	                	
+	                	//logger.info("[Switch= {}][SrcMAC = {}] ACK", dpid.toString(), srcMac.toString());
+	                	int c = this.getNumIllegitimateRequests(pendingList, srcMac);
+	                	logger.info("[SrcMAC = {}] Before ACK: {}", srcMac.toString(), c);
 	                	this.removePendingListRecord(srcIp, dstIp, srcMac, dstMac);
 	                	
+	                	c = this.getNumIllegitimateRequests(pendingList, srcMac);
+	                	logger.info("[SrcMAC = {}] After ACK: {}", srcMac.toString(), c);
+	                	
 	                	// Install permanent forwarding rule between client and server
+						this.installPermanentForwardingRules(srcDpid, dstDpid, IOFSwitch sw);
 	                }
 	            }
 	        }
-	  
-	        
-//	        // Create a new packet-out message to send the packet back out
-//	        OFPacketOut po = sw.getOFFactory().buildPacketOut()
-//	            .setData(packetData)
-//	            .setActions(Collections.singletonList(sw.getOFFactory().actions().output(outPort, 0xffFFffFF)))
-//	            .setInPort(inPort)
-//	            .build();
-//
-//	        // Send the packet-out message to the switch
-//	        sw.write(po);
-//	        
-	        
 	        
 	        break;
 	    default:
@@ -220,18 +231,26 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 		return false;
 	}
 	
+	private void installTemporaryForwardingRules(DatapathId srcDpid, DatapathId dstDpid, IOFSwitch sw) {
+
+	}
+
+	private void installPermanentForwardingRules(DatapathId srcDpid, DatapathId dstDpid, IOFSwitch sw) {
+		
+	}
+
 	private int getNumIllegitimateRequests(List<Connection> pendingList, MacAddress srcMac) {
 		int numIllegitRequests = 0;
 		
 		// Loop through all records in the pending_list
 		for (Connection record : pendingList) {
 			
+			String status = record.getStatus();
+			
 			// Check if the record is an illegit requests
-			if (record.getStatus() == "SYN" || record.getStatus() == "SYN-ACK" || record.getStatus() == "RST") {
+			if (status.equals("SYN") || status.equals("SYN-ACK") || status.equals("RST")) {
 		
-				
 				// Check if it comes from the specified client
-				//logger.info("record.getSrcMac() = {}  srcMac = {}", record.getSrcMac().toString(), srcMac.toString());
 				if (record.getSrcMac().equals(srcMac)) {
 					numIllegitRequests++;
 				}
@@ -246,10 +265,7 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 		for (Connection record : pendingList) {
 			
 			// Get the matching record
-			if (record.getSrcIp() == srcIp &&
-				record.getDstIp() == dstIp && 
-				record.getSrcMac() == srcMac &&
-				record.getDstMac() == dstMac) {
+			if (record.getSrcMac().equals(srcMac)) {
 				
 				// Change the matching record's status
 				synchronized(pendingList) {
@@ -260,55 +276,18 @@ public class ModifiedSLICOTS implements IOFMessageListener, IFloodlightModule {
 	}
 	
 	private void removePendingListRecord(IPv4Address srcIp, IPv4Address dstIp, MacAddress srcMac, MacAddress dstMac) {
-		// Loop through all records in the pending_list
-		for (Connection record : pendingList) {
-			
-			// Get the matching record
-			if (record.getSrcIp() == srcIp &&
-				record.getDstIp() == dstIp && 
-				record.getSrcMac() == srcMac &&
-				record.getDstMac() == dstMac) {
-				
-				// Remove the specified record
-				synchronized(pendingList) {
-					pendingList.remove(record);
-				}
-			}
-		}
+
+	    synchronized (pendingList) {
+	        Iterator<Connection> iter = pendingList.iterator();
+	        while (iter.hasNext()) {
+	            Connection record = iter.next();
+	            if (record.getSrcMac().equals(srcMac)) {
+	                iter.remove();
+	                break;
+	            }
+	        }
+	    }
+		
 	}
-
-//	public void installForwardingRules(DatapathId srcDpid, DatapathId dstDpid, OFPort inPort, OFPort outPort, int hardTimeout) {
-//	    IOFSwitch sw;
-//		// Create OFFactory object for creating flow rule
-//	    OFFactory factory = sw.getOFFactory();
-//
-//	    // Add flow rule on first switch connected to source host to forward packets to the controller
-//	    OFMatch match = factory.buildMatch().setExact(MatchField.IN_PORT, inPort).build();
-//	    OFInstructionGotoTable goToTable = factory.instructions().gotoTable(TableId.of(1));
-//	    OFInstructionApplyActions applyActions = factory.instructions().applyActions(Collections.singletonList((OFAction) factory.actions().output(OFPort.CONTROLLER, Integer.MAX_VALUE)));
-//	    OFFlowAdd flowAdd = factory.buildFlowAdd().setMatch(match).setHardTimeout(hardTimeout).setInstructions(Arrays.asList(goToTable, applyActions)).build();
-//	    IOFSwitch firstSwitch = switchService.getSwitch(srcDpid);
-//	    firstSwitch.write(flowAdd);
-//
-//	    // Add flow rule on switches along the path from source host to destination host
-//	    DatapathId currDpid = srcDpid;
-//	    while (!currDpid.equals(dstDpid)) {
-//	        // Get next hop switch and out port
-//	        Link link = linkService.getEgressLinks(currDpid).get(0);
-//	        currDpid = link.getDst().getDpid();
-//	        OFPort outPort = link.getDstPort();
-//
-//	        // Build match for current switch and create flow rule
-//	        OFMatch match = factory.buildMatch().setExact(MatchField.IN_PORT, inPort).setExact(MatchField.ETH_DST, dstMac).build();
-//	        OFInstructionApplyActions applyActions = factory.instructions().applyActions(Collections.singletonList((OFAction) factory.actions().output(outPort, Integer.MAX_VALUE)));
-//	        OFFlowAdd flowAdd = factory.buildFlowAdd().setMatch(match).setHardTimeout(hardTimeout).setInstructions(Collections.singletonList((OFInstruction) applyActions)).build();
-//	        IOFSwitch currSwitch = switchService.getSwitch(currDpid);
-//	        currSwitch.write(flowAdd);
-//
-//	        // Update in port for next hop
-//	        inPort = outPort;
-//	    }
-//	}
-
 
 }
